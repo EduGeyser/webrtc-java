a860a0cbb69493e15de6ca73433672ec58d1f57d
SendableMetatype <263203301+SendableMetatype@users.noreply.github.com>
2026-09-10 09:13:41 +0000

    Prepare for next development iteration

--- docs/.vitepress/versions.ts
+++ docs/.vitepress/versions.ts
@@ -1,4 +1,4 @@
 export const PROJECT_VARS: Record<string, string> = {
-	VERSION: '0.16.0',
-	VERSION_SNAPSHOT: '0.17.0-SNAPSHOT'
+	VERSION: '0.17.0-sm.1',
+	VERSION_SNAPSHOT: '0.17.0-sm.2-SNAPSHOT'
 };
